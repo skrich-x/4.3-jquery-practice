0926fa6d67d6f80377ae5b99408a6435f4c6aca2
Kyle Richardson <s.k.richardson7@gmail.com>
2015-05-27 21:15:31 -0400

    Jquery fully functional. Implementing Styles used slideToggle

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,13 @@
   'use strict';
 
   $(document).ready(function(){
-    $(subCategories)
-
   $(".subCategories").hide();
 
+});
+
   $('.headers').click(function(){
-    $(".subCategories").siblings(".subCategories").slideUp(400,function(){});
-    $(this).next().slideDown(400,function(){});
+    $(".subCategories").siblings(".subCategories").slideToggle(350,function(){});
+    $(this).next().slideToggle(350,function(){});
   });
 
-
-
-
-  });
 })();
